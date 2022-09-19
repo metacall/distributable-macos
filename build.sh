0c32b4d4d0e824867a8ba7ad9c46cd45042a57ed
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2022-09-19 22:34:56 +0200

    Solve bug in get latest release.

--- build.sh
+++ build.sh
@@ -27,9 +27,9 @@ download() {
 }
 
 get_latest_release() {
-  curl --silent "https://api.github.com/repos/$1/releases/latest" # Get latest release from GitHub api
-    | grep '"tag_name":'                                          # Get tag line
-    | sed -E 's/.*"([^"]+)".*/\1/'                                # Pluck JSON value
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                            # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
 }
 
 download_from_github() {
