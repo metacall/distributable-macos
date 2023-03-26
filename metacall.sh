172d83182ab9849361c7ec51faa91855b5acfc7d
AsyncDeveloper245 <AsyncDeveloper245@users.noreply.github.com>
2023-03-26 09:29:38 +0100

    check package manager is present in metacall environment

--- metacall.sh
+++ metacall.sh
@@ -3,16 +3,28 @@
 LOC=metacall
 PATH=$PATH:$LOC:$LOC/lib
 PYTHONHOME=$LOC/runtimes/python
-PIP_TARGET=$LOC/runtimes/python/Pip
+PIP_TARGET=$LOC/runtimes/python/pip
 # Python
 PATH=$PATH:$LOC/runtimes/python:$LOC/runtimes/python/Scripts
 # NodeJS
 PATH=$PATH:$LOC/lib/runtimes/nodejs:$LOC/lib/runtimes/nodejs/lib
 # DotNet Core
 PATH=$PATH:$LOC/lib/runtimes/dotnet:$LOC/lib/runtimes/dotnet/host/fxr/ # TODO: Add version
+# Ruby
+PATH=$PATH:$LOC/lib/runtimes/ruby/bin:$LOC/lib/runtimes/ruby/bin/ruby_builtin_dll
+
+
 
 # Check if it is running a package manager and execute it
 # TODO
+if [[ "$@" =~ "pip" ]]; then
+$LOC/runtimes/python/bin/pip $@
+elif [[ "$@" =~ "npm" ]]; then
+$LOC/lib/runtimes/nodejs/bin/npm $@
+else
+echo "No package manager detected. Executing MetaCall CLI."
+fi
+
 
 # MetaCall Environment
 CORE_ROOT=$LOC/runtimes/dotnet/shared/Microsoft.NETCore.App/ # TODO: Add version
