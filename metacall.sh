74cf7844b49b35e4de5ebf0db297bcae78918ff9
Omoleye Julius Fortunate <79061404+AsyncDeveloper245@users.noreply.github.com>
2023-03-28 21:30:19 +0100

    Check Running Package Manager (#8)
    * check package manager is present in metacall environment
    
    * add package manager paths
    
    ---------
    
    Co-authored-by: AsyncDeveloper245 <AsyncDeveloper245@users.noreply.github.com>

--- metacall.sh
+++ metacall.sh
@@ -3,16 +3,46 @@
 LOC=metacall
 PATH=$PATH:$LOC:$LOC/lib
 PYTHONHOME=$LOC/runtimes/python
-PIP_TARGET=$LOC/runtimes/python/Pip
+PIP_TARGET=$LOC/runtimes/python/pip
 # Python
 PATH=$PATH:$LOC/runtimes/python:$LOC/runtimes/python/Scripts
 # NodeJS
 PATH=$PATH:$LOC/lib/runtimes/nodejs:$LOC/lib/runtimes/nodejs/lib
 # DotNet Core
 PATH=$PATH:$LOC/lib/runtimes/dotnet:$LOC/lib/runtimes/dotnet/host/fxr/ # TODO: Add version
+# Ruby
+PATH=$PATH:$LOC/lib/runtimes/ruby/bin:$LOC/lib/runtimes/ruby/bin/ruby_builtin_dll
+
+
+# Package Managers Paths
+pip_path="$LOC/runtimes/python/Pip/bin/pip"
+pip3_path="$LOC/runtimes/python/Pip/bin/pip3"
+npm_path="$LOC/runtimes/nodejs/npm"
+npx_path="$LOC/runtimes/nodejs/npx"
+bundle_path="$LOC/runtimes/ruby/bin/bundle"
+bundler_path="$LOC/runtimes/ruby/bin/bundler"
+erb_path="$LOC/runtimes/ruby/bin/erb"
+gem_path="$LOC/runtimes/ruby/bin/gem"
+irb_path="$LOC/runtimes/ruby/bin/irb"
+racc_path="$LOC/runtimes/ruby/bin/racc"
+rake_path="$LOC/runtimes/ruby/bin/rake"
+rbs_path="$LOC/runtimes/ruby/bin/rbs"
+rdbg_path="$LOC/runtimes/ruby/bin/rdbg"
+rdoc_path="$LOC/runtimes/ruby/bin/rdoc"
+ri_path="$LOC/runtimes/ruby/bin/ri"
+typeprof_path="$LOC/runtimes/ruby/bin/typeprof"
+
 
 # Check if it is running a package manager and execute it
 # TODO
+if [[ "$@" =~ "pip" ]]; then
+$LOC/runtimes/python/bin/pip $@
+elif [[ "$@" =~ "npm" ]]; then
+$LOC/lib/runtimes/nodejs/bin/npm $@
+else
+echo "No package manager detected. Executing MetaCall CLI."
+fi
+
 
 # MetaCall Environment
 CORE_ROOT=$LOC/runtimes/dotnet/shared/Microsoft.NETCore.App/ # TODO: Add version
