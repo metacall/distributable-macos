cddddaed2a64c6e550c4563742d0f8a8d3f35218
Prakhar Gurunani <prakhargurunani@gmail.com>
2024-07-18 00:52:46 +0530

    wip: tests

---
+++ test.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# The format of commands (i.e tests/node/commands.txt) must always contain a new line at the end
+
+loc="$(dirname "$0")/tests"
+
+echo "NodeJS tests"
+export LOADER_SCRIPT_PATH="$loc/node"
+echo "Npm Test"
+metacall npm install metacall > out.txt
+if [ $? -eq 1 ]; then
+    cat out.txt
+    echo "Test suite failed"
+    rm out.txt
+    exit 1
+fi
+cat out.txt
+echo "Successful!!"
+echo "Node metacall test"
+cat "$loc/node/commands.txt" | metacall > out.txt
+if [ $? -eq 1 ]; then
+    cat out.txt
+    echo "Test suite failed"
+    rm out.txt
+    exit 1
+fi
+if ! grep -q "366667" out.txt; then
+    cat out.txt
+    echo "Test suite failed"
+    rm out.txt
+    exit 1
+fi
+cat out.txt
+echo "Successful!!"
+
+echo "Python tests"
+export LOADER_SCRIPT_PATH="$loc/python"
+
+PYTHONHOME="$(dirname "$0")/metacall/runtimes/python"
+PIP_TARGET="$PYTHONHOME/Pip"
+PATH="$PYTHONHOME:$PYTHONHOME/Scripts:$PATH"
+"$PYTHONHOME/python" -m pip install --upgrade --force-reinstall pip
+
+echo "Pip Test"
+metacall pip install metacall > out.txt
+if [ $? -eq 1 ]; then
+    cat out.txt
+    echo "Test suite failed"
+    rm out.txt
+    exit 1
+fi
+cat out.txt
+echo "Successful!!"
+echo "Python metacall test"
+cat "$loc/python/commands.txt" | metacall > out.txt
+if [ $? -eq 1 ]; then
+    cat out.txt
+    echo "Test suite failed"
+    rm out.txt
+    exit 1
+fi
+if ! grep -q "Hello World" out.txt; then
+    cat out.txt
+    echo "Test suite failed"
+    rm out.txt
+    exit 1
+fi
+cat out.txt
+echo "Successful!!"
+
+rm out.txt
+exit 0
