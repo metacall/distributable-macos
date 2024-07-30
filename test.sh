6a4d84f9139f28aeeacc44054859d2ddc3e5d627
Vicente Eduardo Ferrer Garcia <7854099+viferga@users.noreply.github.com>
2024-07-30 17:36:28 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# TODO: Implement tests properly and incrementally
+
 # The format of commands (i.e tests/node/commands.txt) must always contain a new line at the end
 
 loc="$(dirname "$0")/tests"
@@ -33,41 +35,38 @@ loc="$(dirname "$0")/tests"
 # cat out.txt
 # echo "Successful!!"
 
-echo "Python tests"
-export LOADER_SCRIPT_PATH="$loc/python"
+# echo "Python tests"
+# export LOADER_SCRIPT_PATH="$loc/python"
 
-# TODO: We should put this into the launcher
-PYTHONHOME="$(dirname "$0")/metacall/runtimes/python"
-PIP_TARGET="$PYTHONHOME/Pip"
-PATH="$PYTHONHOME:$PYTHONHOME/Scripts:$PATH"
-"$PYTHONHOME/python" -m pip install --upgrade --force-reinstall pip
+# # TODO: We should put this into the launcher
+# PYTHONHOME="$(dirname "$0")/metacall/runtimes/python"
+# PIP_TARGET="$PYTHONHOME/Pip"
+# PATH="$PYTHONHOME:$PYTHONHOME/Scripts:$PATH"
+# "$PYTHONHOME/python" -m pip install --upgrade --force-reinstall pip
 
-echo "Pip Test"
-metacall pip install metacall > out.txt
-if [ $? -eq 1 ]; then
-    cat out.txt
-    echo "Test suite failed"
-    rm out.txt
-    exit 1
-fi
-cat out.txt
-echo "Successful!!"
-echo "Python metacall test"
-cat "$loc/python/commands.txt" | metacall > out.txt
-if [ $? -eq 1 ]; then
-    cat out.txt
-    echo "Test suite failed"
-    rm out.txt
-    exit 1
-fi
-if ! grep -q "Hello World" out.txt; then
-    cat out.txt
-    echo "Test suite failed"
-    rm out.txt
-    exit 1
-fi
-cat out.txt
-echo "Successful!!"
-
-rm out.txt
-exit 0
+# echo "Pip Test"
+# metacall pip install metacall > out.txt
+# if [ $? -eq 1 ]; then
+#     cat out.txt
+#     echo "Test suite failed"
+#     rm out.txt
+#     exit 1
+# fi
+# cat out.txt
+# echo "Successful!!"
+# echo "Python metacall test"
+# cat "$loc/python/commands.txt" | metacall > out.txt
+# if [ $? -eq 1 ]; then
+#     cat out.txt
+#     echo "Test suite failed"
+#     rm out.txt
+#     exit 1
+# fi
+# if ! grep -q "Hello World" out.txt; then
+#     cat out.txt
+#     echo "Test suite failed"
+#     rm out.txt
+#     exit 1
+# fi
+# cat out.txt
+# echo "Successful!!"
