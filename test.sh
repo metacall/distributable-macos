73157eb9a6eaf3ba3a709b59bad35535960b81f3
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2024-08-01 00:04:47 +0200

    Add a better way for testing.

--- test.sh
+++ test.sh
@@ -1,17 +1,40 @@
 #!/bin/sh
 
+repl_test() {
+    echo "$1"
+    TEST_COMMAND="`echo $2 | metacall`"
+    if echo "$TEST_COMMAND" | grep -q "$3"; then
+        echo "Passed"
+    else
+        echo "Failed"
+        echo "Commands:"
+        echo "$2"
+        echo "Expected: $3"
+        echo "Received: $TEST_COMMAND"
+        exit 1
+    fi
+}
+
 echo "Python Tests"
 
-echo "Running Python Reverse Words Test"
-echo 'load py tests/python/test.py\ninspect\ncall reverse_words("hello world")\nexit' | metacall | grep "dlrow olleh" || exit 1
+repl_test \
+    "Running Python Reverse Words Test" \
+    'load py tests/python/test.py\ninspect\ncall reverse_words("hello world")\nexit' \
+    "dlrow olleh"
 
-echo "Running Python Factorial Test"
-echo "load py tests/python/test.py\ninspect\ncall factorial(3)\nexit" | metacall | grep "6" || exit 1
+repl_test \
+    "Running Python Factorial Test" \
+    "load py tests/python/test.py\ninspect\ncall factorial(3)\nexit" \
+    "6"
 
 echo "NodeJS Tests"
 
-echo "Running NodeJS Reverse Words Test"
-echo 'load node tests/node/test.js\ninspect\ncall reverseWord("hello world")\nexit' | metacall | grep "dlrow olleh" || exit 1
+repl_test \
+    "Running NodeJS Reverse Words Test" \
+    'load node tests/node/test.js\ninspect\ncall reverseWord("hello world")\nexit' \
+    "dlrow olleh"
 
-echo "Running NodeJS Factorial Test"
-echo "load node tests/node/test.js\ninspect\ncall factorial(3)\nexit" | metacall | grep "6" || exit 1
+repl_test \
+    "Running NodeJS Factorial Test" \
+    "load node tests/node/test.js\ninspect\ncall factorial(3)\nexit" \
+    "6"
