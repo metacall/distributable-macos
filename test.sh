045f6de74a6eae2eb7bcb82464ecbccfef6de6d6
Prakhar Gurunani <prakhargurunani@gmail.com>
2024-07-31 14:51:06 +0530

    feat: tests

--- test.sh
+++ test.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # Python tests
-py_reverse_words=$(echo 'load py tests/python/test.py\ncall reverse_words("hello world")\nexit' | metacall | grep 'dlrow olleh')
+py_reverse_words=$(echo 'load py tests/python/test.py\ninspect\ncall reverse_words("hello world")\nexit' | metacall | grep 'dlrow olleh')
 if [ -z "$py_reverse_words" ]; then
     exit 1
 fi
 
-py_factorial=$(echo 'load py tests/python/test.py\ncall factorial(3)\nexit' | metacall | grep '6')
+py_factorial=$(echo 'load py tests/python/test.py\ninspect\ncall factorial(3)\nexit' | metacall | grep '6')
 if [ -z "$py_factorial" ]; then
     exit 1
 fi
 
 # NodeJS tests
-js_reverse_words=$(echo 'load node tests/node/test.js\ncall reverseWord("hello world")\nexit' | metacall | grep 'dlrow olleh')
+js_reverse_words=$(echo 'load node tests/node/test.js\ninspect\ncall reverseWord("hello world")\nexit' | metacall | grep 'dlrow olleh')
 if [ -z "$js_reverse_words" ]; then
     exit 1
 fi
 
-js_factorial=$(echo 'load node tests/node/test.js\ncall factorial(3)\nexit' | metacall | grep '6')
+js_factorial=$(echo 'load node tests/node/test.js\ninspect\ncall factorial(3)\nexit' | metacall | grep '6')
 if [ -z "$js_factorial" ]; then
     exit 1
 fi
