41a7ad7e1021261ee7b085c496acc5803ac3d956
Vicente Eduardo Ferrer Garcia <7854099+viferga@users.noreply.github.com>
2024-07-31 14:15:17 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,19 +1,17 @@
-#!/usr/bin/env bash
-
-set -euxo pipefail
+#!/bin/sh
 
 echo "Python Tests"
 
 echo "Running Python Reverse Words Test"
-echo 'load py tests/python/test.py\ninspect\ncall reverse_words("hello world")\nexit' | metacall | grep "dlrow olleh"
+echo 'load py tests/python/test.py\ninspect\ncall reverse_words("hello world")\nexit' | metacall | grep "dlrow olleh" || exit 1
 
 echo "Running Python Factorial Test"
-echo "load py tests/python/test.py\ninspect\ncall factorial(3)\nexit" | metacall | grep "6"
+echo "load py tests/python/test.py\ninspect\ncall factorial(3)\nexit" | metacall | grep "6" || exit 1
 
 echo "NodeJS Tests"
 
 echo "Running NodeJS Reverse Words Test"
-echo 'load node tests/node/test.js\ninspect\ncall reverseWord("hello world")\nexit' | metacall | grep "dlrow olleh"
+echo 'load node tests/node/test.js\ninspect\ncall reverseWord("hello world")\nexit' | metacall | grep "dlrow olleh" || exit 1
 
 echo "Running NodeJS Factorial Test"
-echo "load node tests/node/test.js\ninspect\ncall factorial(3)\nexit" | metacall | grep "6"
+echo "load node tests/node/test.js\ninspect\ncall factorial(3)\nexit" | metacall | grep "6" || exit 1
