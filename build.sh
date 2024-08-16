08534aad38ad8edac8df040ce83cec6fc4619fce
Prakhar Gurunani <prakhargurunani@gmail.com>
2024-08-16 20:23:38 +0530

    feat: clean up .tgz and create distributable

--- build.sh
+++ build.sh
@@ -39,9 +39,16 @@ function architecture() {
 METACALL_VERSION=`brew info metacall | grep -i "stable" | awk '{print $4}' | sed 's/.$//'`
 METACALL_ARCH=`architecture`
 
-brew tap --verbose FirePing32/brew-pkg
-brew install --verbose --HEAD FirePing32/brew-pkg/brew-pkg
+brew tap --verbose metacall/brew-pkg
+brew install --verbose --HEAD metacall/brew-pkg/brew-pkg
 mkdir release && cd release
 brew pkg --with-deps --compress metacall
 mv metacall-${METACALL_VERSION}.pkg metacall-tarball-macos-${METACALL_ARCH}.pkg
-mv metacall-${METACALL_VERSION}.tgz metacall-tarball-macos-${METACALL_ARCH}.tgz
+
+# Extract the .tgz file
+tar -xzvf metacall-${METACALL_VERSION}.tgz
+mkdir distributable
+cp -r private/tmp/brew-pkg[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]-[0-9]*-[a-z0-9]*/opt/homebrew/Cellar/metacall/[0-9]*.[0-9]*.[0-9]* distributable/metacall-core
+cp private/tmp/brew-pkg[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]-[0-9]*-[a-z0-9]*/opt/homebrew/bin/metacall distributable/
+sed -i '' '2s|^PREFIX=.*|PREFIX=metacall-core|' "distributable/metacall"
+tar -czf metacall-tarball-macos-${METACALL_ARCH}.tgz distributable
