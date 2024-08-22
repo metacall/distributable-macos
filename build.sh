1c93f82b05af00adaa44d479e981cede1e80b545
Prakhar Gurunani <prakhargurunani@gmail.com>
2024-08-22 15:54:40 +0530

    fix: lib regex

--- build.sh
+++ build.sh
@@ -72,7 +72,11 @@ sed -i '' '2s|^PREFIX=.*|PREFIX=metacall-core|' "distributable/metacall"
 # Change path of shared libraries
 change_library_path() {
   loader=$1
-  lib_regex="opt/homebrew/opt"
+  if [ "$INSTALL_DIR" = "opt/homebrew" ]; then
+      lib_regex="opt/homebrew/opt"
+  else
+      lib_regex="usr/local"
+  fi
   metacall_lib=distributable/metacall-core/lib/lib${loader}_loader.so
 
   old_lib=$(otool -L "$metacall_lib" | grep -E "$lib_regex" | awk '{print $1}')
